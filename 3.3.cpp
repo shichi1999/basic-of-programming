5f83f5654de6e6853ac736b70b54588b937f77e0
shichi1999 <116809457+shichi1999@users.noreply.github.com>
2022-10-29 20:04:52 +0530

    Add files via upload

--- 3.3.cpp
+++ 3.3.cpp
@@ -1,15 +1,25 @@
 #include <Arduino.h>
 //Declaring all variables as integers
-int Z=0,Y=0,X=0,W=0;
-int D,C,B,A;
+int A,B,C,D,a,b,c,d,e,f,g;
 
 //Code released under GNU GPL.  Free to use for anything.
-void disp_7447(int D, int C, int B, int A)
+void disp_7447(int A,int B,int C,int D)
 {
-  digitalWrite(2, A); //LSB
-  digitalWrite(3, B); 
-  digitalWrite(4, C); 
-  digitalWrite(5, D); //MSB
+  
+  a=(A&&!B&&!C&&!D) || (!A&&C&&);
+  b=(!A&&B&&C) || (A&&!B&&C);
+  c=(!A&&B&&!C);
+  d=(!A&&!B&&C)  (A&&!B&&!C)  (A&&B&&C);
+  e=(!B&&C) || (A);
+  f=(A&&!C&&!D)  (B&&!C)  (A&&B);
+  g=(A&&B&&C) || (!B&&!C&&!D);
+  digitalWrite(2, a); //LSB
+  digitalWrite(3, b); 
+  digitalWrite(4, c); 
+  digitalWrite(5, d);
+  digitalWrite(6, e);
+  digitalWrite(7, f); 
+  digitalWrite(8, g); //MSB
 
 }
 // the setup function runs once when you press reset or power the board
@@ -18,26 +28,21 @@ void setup() {
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
-    pinMode(6, INPUT);  
-    pinMode(7, INPUT);
-    pinMode(8, INPUT);
+    pinMode(6, OUTPUT);  
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
     pinMode(9, INPUT);
+    pinMode(10, INPUT);
+    pinMode(11, INPUT);
+    pinMode(12, INPUT);
+    
 }
 
 // the loop function runs over and over again forever
 void loop() {
-
- /* digitalWrite(6, W); //LSB
-  digitalWrite(7, X); 
-  digitalWrite(8, Y); 
-  digitalWrite(9, Z);*/
-  A=!W;
-  B=(W&&!X&&!Y&&!Z) || (!W&&X&&!Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  C=(W&&X&&!Y&&!Z) || (!W&&!X&&Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  D = (W&&X&&Y&&!Z)||(!W&&!X&&!Y&&Z);
-  
-disp_7447(D,C,B,A);  
+ A= digitalRead(9);
+ B= digitalRead(10);
+ C= digitalRead(11);
+ D= digitalRead(12);
+disp_7447(A,B,C,D);  
 }
-//&& is the AND operation
-// || is the OR operation
-// ! is the NOT operation
